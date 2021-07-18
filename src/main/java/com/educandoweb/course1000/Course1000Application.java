98e07577ddc17c56ab6d7bfbd2f9109ed9c6f0b3
Maycol Michel Mohr <maycolmohr@yahoo.com.br>
2021-07-18 14:29:51 +0200

    Project created

---
+++ src/main/java/com/educandoweb/course1000/Course1000Application.java
@@ -0,0 +1,13 @@
+package com.educandoweb.course1000;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Course1000Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Course1000Application.class, args);
+	}
+
+}
